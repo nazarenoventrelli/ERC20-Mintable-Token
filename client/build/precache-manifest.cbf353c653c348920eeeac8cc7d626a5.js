a184ef9e8dc2f7f3a088d0a26911fb32be849134
nazarenoventrelli <40725187+nazarenoventrelli@users.noreply.github.com>
2021-12-14 19:06:02 -0300

    compile

---
+++ client/build/precache-manifest.cbf353c653c348920eeeac8cc7d626a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a007f24daf38e346196b6938444bbe",
+    "url": "/ERC20-Mintable-Token/client/index.html"
+  },
+  {
+    "revision": "b4ce0c6aa7aa259088c6",
+    "url": "/ERC20-Mintable-Token/client/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "e34c5ffa3c6c0d2407ed",
+    "url": "/ERC20-Mintable-Token/client/static/js/2.4e3779ee.chunk.js"
+  },
+  {
+    "revision": "b4ce0c6aa7aa259088c6",
+    "url": "/ERC20-Mintable-Token/client/static/js/main.b760d08c.chunk.js"
+  },
+  {
+    "revision": "2e9a6d14a46788707d04",
+    "url": "/ERC20-Mintable-Token/client/static/js/runtime-main.0c602da2.js"
+  }
+]);
