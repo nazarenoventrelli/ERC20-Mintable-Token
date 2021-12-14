a7854cdce18783ffcba122996c7f84aeaa627a59
nazarenoventrelli <40725187+nazarenoventrelli@users.noreply.github.com>
2021-12-14 19:25:51 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ERC20-Mintable-Token/client/precache-manifest.cbf353c653c348920eeeac8cc7d626a5.js"
+  "/ERC20-Mintable-Token/client/build/precache-manifest.788317975d3b2ad5c92e63bbb76c5434.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ERC20-Mintable-Token/client/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ERC20-Mintable-Token/client/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
